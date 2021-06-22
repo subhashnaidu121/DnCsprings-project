bffe821617e919216392e62802d985d4344938d7
SUBHASH <subhashnaidu121@gmail.com>
2021-06-22 15:32:22 +0530

    commit

---
+++ crudSBThyme/src/main/java/com/vm/CrudSbThymeApplication.java
@@ -0,0 +1,13 @@
+package com.vm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSbThymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSbThymeApplication.class, args);
+	}
+
+}
